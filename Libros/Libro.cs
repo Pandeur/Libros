7cebd58f1b7deda9cf8e2070dc00b31c370b4781
Pandeur <User@CTE-LAB304-PC06>
2026-01-14 15:46:07 -0500

    jeje

--- Libros/Libro.cs
+++ Libros/Libro.cs
@@ -6,5 +6,13 @@ namespace Libros
 {
     internal class Libro
     {
+        public string Nombre {  get; set; }
+        public string Autor {  get; set; }
+        public Libro() { }
+        public Libro(string nombre, string autor)
+        {
+            Nombre = nombre;
+            Autor = autor;
+        }
     }
 }
